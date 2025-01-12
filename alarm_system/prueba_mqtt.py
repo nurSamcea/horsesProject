a83839b71ee4fa830030de1ba14bbf51e58b6e6c
nurSamcea <54370636+nurSamcea@users.noreply.github.com>
2025-01-12 16:03:09 +0100

    Create prueba_mqtt.py

---
+++ alarm_system/prueba_mqtt.py
@@ -0,0 +1,23 @@
+import paho.mqtt.client as mqtt
+import json
+
+# Configuración
+broker = "srv-iot.diatel.upm.es"
+port = 1883
+topic = "v1/devices/me/telemetry"
+access_token = "7jpi6hyp0jzolihttq45"
+
+# Crear cliente MQTT
+client = mqtt.Client()
+client.username_pw_set(access_token)
+
+# Conectar al broker
+client.connect(broker, port, 60)
+
+# Publicar mensaje
+data = {"temperature": 25}
+client.publish(topic, json.dumps(data))
+print("Mensaje enviado:", data)
+
+# Cerrar conexión
+client.disconnect()
