e241e263aed61f1bd6845e70aaa1d3ee3830f5c0
Nuria Alvarez Rio <nuriaalvarezrio@gmail.com>
2025-01-16 18:59:24 +0100

    feat: update current version with MQTT

--- horsesApp/app/build.gradle.kts
+++ horsesApp/app/build.gradle.kts
@@ -9,7 +9,6 @@ android {
     defaultConfig {
         applicationId = "com.example.horsesapp"
         minSdk = 24
-        compileSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -30,6 +29,11 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+    packaging {
+        resources.excludes.add("/META-INF/{AL2.0,LGPL2.1}")
+        resources.excludes.add("META-INF/INDEX.LIST")
+        resources.excludes.add("META-INF/io.netty.versions.properties")
+    }
 }
 
 dependencies {
@@ -38,10 +42,8 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
-    implementation("org.eclipse.paho:org.eclipse.paho.android.service:1.1.1")
-    implementation("androidx.localbroadcastmanager:localbroadcastmanager:1.1.0")
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
+    implementation("com.hivemq:hivemq-mqtt-client:1.3.3")
 }
--- horsesApp/app/src/main/java/com/example/horsesapp/MainActivity.java
+++ horsesApp/app/src/main/java/com/example/horsesapp/MainActivity.java
@@ -6,6 +6,8 @@ package com.example.horsesapp;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
@@ -13,14 +15,31 @@ import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.hivemq.client.mqtt.MqttClient;
+import com.hivemq.client.mqtt.mqtt3.Mqtt3AsyncClient;
+
 public class MainActivity extends AppCompatActivity {
 
-    private final String[] horses = {"Caballo 1", "Caballo 2", "Caballo 3", "Caballo 4"};
+    private final String[] horses = {"Horse 0", "Horse 1", "Horse 2", "Horse 3", "Horse 4",
+            "Horse 5", "Horse 6", "Horse 7", "Horse 8", "Horse 9"};
+
+    String TAG = "HORSE APP MQTT";
+    String serverHost = "srv-iot.diatel.upm.es";
+    int serverPort = 8883;
+    String access_token = "n8qc2l1jo0cpt5w6bhm6";
+
+    String subscriptionTopic = "v1/devices/me/attributes";
+    String publishingTopic = "v1/devices/me/telemetry";
+
+    Mqtt3AsyncClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        createMQTTclient();
+        connectToBroker();
 
         // Inicializar la lista de caballos
         ListView horseListView = findViewById(R.id.horse_list_view);
@@ -43,16 +62,117 @@ public class MainActivity extends AppCompatActivity {
         simulateAlert();
     }
 
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disconnectFromBroker();
+    }
+
+    void createMQTTclient() {
+        client = MqttClient.builder()
+                .useMqttVersion3()
+                .identifier("veterinaryApp")
+                .serverHost(serverHost)
+                .serverPort(serverPort)
+                .sslWithDefaultConfig() // Configuración para usar TLS
+                .buildAsync();
+    }
+
+    void connectToBroker() {
+        if (client != null) {
+            client.connectWith()
+                    .simpleAuth()
+                    .username(access_token)
+                    .applySimpleAuth()
+                    .send()
+                    .whenComplete((connAck, throwable) -> {
+                        if (throwable != null) {
+                            Log.d(TAG, "Problem connecting to server:");
+                            Log.d(TAG, throwable.toString());
+                        } else {
+                            Log.d(TAG, "Connected to server");
+                            subscribeToTopic();
+                        }
+                    });
+        } else {
+            Log.d(TAG, "Cannot connect client (null)");
+        }
+    }
+
+    void subscribeToTopic() {
+        client.subscribeWith()
+                .topicFilter(subscriptionTopic)
+                .callback(publish -> {
+                    String receivedMessage = new String(publish.getPayloadAsBytes());
+                    Log.d(TAG, "Message received: " + receivedMessage);
+                })
+                .send()
+                .whenComplete((subAck, throwable) -> {
+                    if (throwable != null) {
+                        Log.d(TAG, "Problem subscribing to topic:");
+                        Log.d(TAG, throwable.toString());
+                    } else {
+                        Log.d(TAG, "Subscribed to topic");
+                    }
+                });
+    }
+
+    private void publishMessage(String payload) {
+        if (payload == null || payload.isEmpty()) {
+            // Si el mensaje está vacío, no hacemos nada
+            Log.d(TAG, "El mensaje está vacío. No se publicará.");
+            return;
+        }
+
+        if (client == null || !client.getState().isConnected()) {
+            // Verifica que el cliente MQTT esté conectado
+            Log.d(TAG, "El cliente MQTT no está conectado. No se puede publicar.");
+            return;
+        }
+
+        client.publishWith()
+                .topic(publishingTopic)
+                .payload(payload.getBytes())
+                .send()
+                .whenComplete((publish, throwable) -> {
+                    if (throwable != null) {
+                        Log.e(TAG, "Error publicando el mensaje en el tópico " + publishingTopic, throwable);
+                    } else {
+                        Log.d(TAG, "Mensaje publicado en el tópico: " + publishingTopic);
+                    }
+                });
+    }
+
     private void simulateAlert() {
-        // Simular una alerta para Caballo 1
+        String alertMessage = "Temperatura alta detectada en Caballo 1.";
+
         new AlertDialog.Builder(this)
                 .setTitle("Alerta: Caballo 1")
-                .setMessage("Temperatura alta detectada en Caballo 1.")
+                .setMessage(alertMessage)
                 .setPositiveButton("Voy en camino", (dialog, which) -> {
                     Toast.makeText(this, "Marcado como en camino.", Toast.LENGTH_SHORT).show();
+                    String defaultPayload = "Caballo 1: Operario en camino. Alerta: " + alertMessage;
+                    publishMessage(defaultPayload); // Publica un mensaje predefinido
                 })
                 .setNegativeButton("Cerrar", (dialog, which) -> dialog.dismiss())
                 .create()
                 .show();
     }
+
+    void disconnectFromBroker() {
+        if (client != null) {
+            client.disconnect()
+                    .whenComplete((result, throwable) -> {
+                        if (throwable != null) {
+                            Log.d(TAG, "Problem disconnecting from server:");
+                            Log.d(TAG, throwable.toString());
+                        } else {
+                            Log.d(TAG, "Disconnected from server");
+                        }
+                    });
+        } else {
+            Log.d(TAG, "Cannot disconnect client (null)");
+        }
+    }
+
 }
